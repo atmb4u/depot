4d2f755c8052739df860650a5b658487c278a764
Anoop Thomas Mathew <atm@profoundis.com>
2014-09-18 21:51:56 +0530

    version changed, depot command works now

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ name='python-depot',
 # Versions should comply with PEP440. For a discussion on single-sourcing
 # the version across setup.py and the project code, see
 # http://packaging.python.org/en/latest/tutorial.html#version
-version='0.9',
-description='Shared variable for python processes',
+version='0.99',
+description='Shared variables for python processes',
 long_description="Easy way to share a python data structure between modules in python using multiprocessing.Manager",
 # The project's main homepage.
-url='https://github.com/atmb4u/python-depot',
+url='https://github.com/atmb4u/depot',
 # Author details
 author='Anoop Thomas Mathew',
 author_email='atmb4u@gmail.com',
 # Choose your license
 license='3-clause BSD License',
-scripts = ['depot'],
+scripts=['depot', 'daemon.py'],
 # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
 classifiers=[
 'Development Status :: 4 - Beta',
@@ -29,5 +29,6 @@ classifiers=[
 'Topic :: Software Development :: Libraries :: Python Modules',
 ],
 # What does your project relate to?
-keywords='depot persistent dictionary development shared variables'
+keywords='depot persistent dictionary development shared variables',
+install_requires=['python-daemon'],
 )
